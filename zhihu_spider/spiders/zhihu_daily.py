e585d7f42cf33eb2900bfb920196ae43470930b2
ArtixZ <artixzhu@gmail.com>
2018-12-04 23:43:29 -0500

    crawl 20 topics

--- zhihu_spider/spiders/zhihu_daily.py
+++ zhihu_spider/spiders/zhihu_daily.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "../config.json")) as topics:
+    topics = json.load(topics)["zhihu_topic"]
 
 class ZhihuDailySpider(scrapy.Spider):
     name = 'zhihu_daily'
@@ -9,24 +13,25 @@ class ZhihuDailySpider(scrapy.Spider):
             'zhihu_spider.pipelines.MongoDBDaily': 800
         }
     }
-    # allowed_domains = ['https://www.zhihu.com/explore']
+    allowed_domains = ['https://www.zhihu.com']
     # start_urls = ['http://https://www.zhihu.com/explore/']
     top_activity = 'https://www.zhihu.com/api/v4/topics/{0}/feeds/top_activity?include=data%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Danswer)%5D.target.content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Danswer)%5D.target.is_normal%2Ccomment_count%2Cvoteup_count%2Ccontent%2Crelevant_info%2Cexcerpt.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Darticle)%5D.target.content%2Cvoteup_count%2Ccomment_count%2Cvoting%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Dpeople)%5D.target.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Danswer)%5D.target.content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F(target.type%3Danswer)%5D.target.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Darticle)%5D.target.content%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dquestion)%5D.target.comment_count&offset={1}&limit={2}'
-
     def start_requests(self):
-        topic_id = 19556664
-        yield scrapy.Request(
-            url=self.top_activity.format(topic_id, 0, 10),
-            method='GET',
-            meta={'topic_id': topic_id, 'count': 0, 'url': self.top_activity.format(topic_id, 0, 10)},
-            # headers=self.headers,
-            dont_filter=True,
-            callback=self.parse_top_activity,)
+        for topic in topics:
+            # import pdb; pdb.set_trace()
+            topic_id = topic["id"]
+            yield scrapy.Request(
+                url=self.top_activity.format(topic_id, 0, 10),
+                method='GET',
+                meta={'topic_id': topic_id, 'count': 0, 'url': self.top_activity.format(topic_id, 0, 10)},
+                # headers=self.headers,
+                dont_filter=True,
+                callback=self.parse_top_activity,)
 
     def parse_top_activity(self, response):
         # from scrapy.shell import inspect_response
         # inspect_response(response, self)
-
+        meta = response.meta
         resJson = json.loads(response.body)
         data = resJson['data']
 
@@ -44,6 +49,7 @@ class ZhihuDailySpider(scrapy.Spider):
                 'voteup_count': target['voteup_count'],
                 'question': target['question'],
                 'updated_time': target['updated_time'],
+                'topic_id': meta['topic_id'],
             }
     
     def parse(self, response):
